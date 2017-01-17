edbb0e0b7487af292ba76d4e42f8b86fc6eceb78
Diego Machado <gigantetsi@gmail.com>
2017-01-17 15:53:26 -0200

    bug generators

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+        //$this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
     }
 }
